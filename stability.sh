dc1cc7e12194c288bfd688b73f4343b06cf2be42
Christian J. Steinmetz <csteinmetz1@gmail.com>
2023-03-31 07:41:56 +0000

    script for downloading data on stability server

--- stability.sh
+++ stability.sh
@@ -4,5 +4,5 @@ cd /scratch
 mkdir csteinmetz1
 cd csteinmetz1
 aws s3 sync s3://stability-aws/MedleyDB ./
-tar -xvf MedleyDB_v1.tar.gz
-tar -xvf MedleyDB_v2.tar.gz
+tar -xvf MedleyDB_v1.tar
+tar -xvf MedleyDB_v2.tar
