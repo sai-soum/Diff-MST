9c61640d413c8be90dbe1e815603d53965e055b0
csteinmetz1 <csteinmetz1@gmail.com>
2023-07-05 09:55:40 +0100

    updating tests

--- tests/test_mst.py
+++ tests/test_mst.py
@@ -12,7 +12,7 @@ sample_rate = 44100
 embed_dim = 128
 num_track_control_params = 27
 num_fx_bus_control_params = 25
-num_master_bus_control_params = 24 
+num_master_bus_control_params = 24
 use_fx_bus = True
 use_master_bus = True
 
@@ -37,6 +37,12 @@ seq_len = 262144
 tracks = torch.randn(bs, num_tracks, seq_len)
 ref_mix = torch.randn(bs, 2, seq_len)
 
-mix, params = model(tracks, ref_mix)
+(
+    mixed_tracks,
+    mix,
+    track_param_dict,
+    fx_bus_param_dict,
+    master_bus_param_dict,
+) = model(tracks, ref_mix)
 print(mix.shape)
 print(mix)
