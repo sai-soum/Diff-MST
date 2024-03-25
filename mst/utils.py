d26ff398fc8ce1e407b4b2d84ae503a091f09007
csteinmetz1 <csteinmetz1@gmail.com>
2024-03-25 08:48:32 +0000

    new model loading and inference func

--- mst/utils.py
+++ mst/utils.py
@@ -3,6 +3,10 @@ import yaml
 import torch
 import random
 import numpy as np
+import pyloudnorm as pyln
+
+from tqdm import tqdm
+from typing import Optional
 from importlib import import_module
 from mst.modules import MixStyleTransferModel
 
@@ -25,11 +29,149 @@ def batch_stereo_peak_normalize(x: torch.Tensor):
     return x_norm
 
 
-def load_model(config_path: str, ckpt_path: str, map_location: str = "cpu"):
+def run_diffmst(
+    tracks: torch.Tensor,
+    ref: torch.Tensor,
+    model: torch.nn.Module,
+    mix_console: torch.nn.Module,
+    track_start_idx: int = 0,
+    ref_start_idx: int = 0,
+):
+    """Run the differentiable mix style transfer model.
+
+    Args:
+        tracks (Tensor): Set of input tracks with shape (bs, num_tracks, 1, seq_len).
+        ref (Tensor): Reference mix with shape (bs, 2, seq_len).
+        model (torch.nn.Module): MixStyleTransferModel instance.
+        mix_console (torch.nn.Module): MixConsole instance.
+        track_start_idx (int, optional): Start index of the track to use. Default: 0.
+        ref_start_idx (int, optional): Start index of the reference mix to use. Default: 0.
+
+    Returns:
+        pred_mix (Tensor): Predicted mix with shape (bs, 2, seq_len).
+        pred_track_param_dict (dict): Dictionary with predicted track parameters.
+        pred_fx_bus_param_dict (dict): Dictionary with predicted fx bus parameters.
+        pred_master_bus_param_dict (dict): Dictionary with predicted master bus parameters.
+    """
+    # ------ defaults ------
+    use_track_input_fader = True
+    use_track_panner = True
+    use_track_eq = True
+    use_track_compressor = True
+    use_fx_bus = False
+    use_master_bus = True
+    use_output_fader = True
+
+    analysis_len = 262144
+    meter = pyln.Meter(44100)
+
+    # crop the input tracks and reference mix to the analysis length
+    if tracks.shape[-1] > analysis_len:
+        analysis_tracks = tracks[
+            ..., track_start_idx : track_start_idx + analysis_len
+        ].clone()
+    else:
+        analysis_tracks = tracks.clone()
+
+    if ref.shape[-1] > analysis_len:
+        analysis_ref = ref[..., ref_start_idx : ref_start_idx + analysis_len]
+    else:
+        analysis_ref = ref.clone()
+
+    # loudness normalize the tracks to -48 LUFS
+    norm_tracks = []
+    norm_analysis_tracks = []
+    track_padding = []
+    for track_idx in range(analysis_tracks.shape[1]):
+        analysis_track = analysis_tracks[:, track_idx : track_idx + 1, :]
+        track = tracks[:, track_idx : track_idx + 1, :]
+        lufs_db = meter.integrated_loudness(
+            analysis_track.squeeze(0).permute(1, 0).numpy()
+        )
+        if lufs_db < -80.0:
+            print(f"Skipping track {track_idx} due to low loudness {lufs_db}.")
+            continue
+
+        lufs_delta_db = -48 - lufs_db
+        analysis_track *= 10 ** (lufs_delta_db / 20)
+        track *= 10 ** (lufs_delta_db / 20)
+
+        norm_analysis_tracks.append(analysis_track)
+        norm_tracks.append(track)
+        track_padding.append(False)
+
+    norm_analysis_tracks = torch.cat(norm_analysis_tracks, dim=1)
+    norm_tracks = torch.cat(norm_tracks, dim=1)
+    print(norm_analysis_tracks.shape, norm_tracks.shape)
+
+    # take only first 16 tracks
+    # norm_tracks = norm_tracks[:, :16, :]
+    # norm_analysis_tracks = norm_analysis_tracks[:, :16, :]
+    print(norm_analysis_tracks.shape, norm_tracks.shape)
+
+    # make tensor contiguous
+    norm_analysis_tracks = norm_analysis_tracks.contiguous()
+    norm_tracks = norm_tracks.contiguous()
+
+    #  ---- run model to estimate mix parmaeters using analysis audio ----
+    pred_track_params, pred_fx_bus_params, pred_master_bus_params = model(
+        norm_analysis_tracks, analysis_ref
+    )
+
+    # ------- generate a mix using the predicted mix console parameters -------
+    # apply with sliding window of 262144 samples with overlap
+    pred_mix = torch.zeros(1, 2, norm_tracks.shape[-1])
+
+    for i in tqdm(range(0, norm_tracks.shape[-1], analysis_len // 2)):
+        norm_tracks_window = norm_tracks[..., i : i + analysis_len]
+        (
+            pred_mixed_tracks,
+            pred_mix_window,
+            pred_track_param_dict,
+            pred_fx_bus_param_dict,
+            pred_master_bus_param_dict,
+        ) = mix_console(
+            norm_tracks_window,
+            pred_track_params,
+            pred_fx_bus_params,
+            pred_master_bus_params,
+            use_track_input_fader=use_track_input_fader,
+            use_track_panner=use_track_panner,
+            use_track_eq=use_track_eq,
+            use_track_compressor=use_track_compressor,
+            use_fx_bus=use_fx_bus,
+            use_master_bus=use_master_bus,
+            use_output_fader=use_output_fader,
+        )
+        if pred_mix_window.shape[-1] < analysis_len:
+            pred_mix_window = torch.nn.functional.pad(
+                pred_mix_window, (0, analysis_len - pred_mix_window.shape[-1])
+            )
+
+        # apply hann window
+        pred_mix_window *= torch.hann_window(pred_mix_window.shape[-1])
+
+        # check length of the mix window
+        output_len = pred_mix[..., i : i + analysis_len].shape[-1]
+
+        # overlap add
+        pred_mix[..., i : i + analysis_len] += pred_mix_window[..., :output_len]
+
+    # crop the mix to the original length
+    pred_mix = pred_mix[..., : norm_tracks.shape[-1]]
+
+    return (
+        pred_mix,
+        pred_track_param_dict,
+        pred_fx_bus_param_dict,
+        pred_master_bus_param_dict,
+    )
+
+
+def load_diffmst(config_path: str, ckpt_path: str, map_location: str = "cpu"):
     with open(config_path) as f:
         config = yaml.safe_load(f)
 
-    # print(config)
     core_model_configs = config["model"]["init_args"]["model"]
 
     module_path, class_name = core_model_configs["class_path"].rsplit(".", 1)
@@ -66,12 +208,12 @@ def load_model(config_path: str, ckpt_path: str, map_location: str = "cpu"):
     )
 
     # create mix console module
-    module_path, class_name = submodule_configs["mix_console"]["class_path"].rsplit(
-        ".", 1
-    )
+    module_path, class_name = config["model"]["init_args"]["mix_console"][
+        "class_path"
+    ].rsplit(".", 1)
     module = import_module(module_path)
     mix_console = getattr(module, class_name)(
-        **submodule_configs["mix_console"]["init_args"]
+        **config["model"]["init_args"]["mix_console"]["init_args"]
     )
 
     checkpoint = torch.load(ckpt_path, map_location=map_location)
@@ -101,10 +243,14 @@ def load_model(config_path: str, ckpt_path: str, map_location: str = "cpu"):
             state_dict[k.replace("model.mix_console.", "", 1)] = v
     mix_console.load_state_dict(state_dict)
 
-    model = MixStyleTransferModel(track_encoder, mix_encoder, controller, mix_console)
+    model = MixStyleTransferModel(
+        track_encoder,
+        mix_encoder,
+        controller,
+    )
     model.eval()
 
-    return model
+    return model, mix_console
 
 
 def denorm(p, p_min=0.0, p_max=1.0):
