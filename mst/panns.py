3d6aab5d5aa7a4fb6dbccd12b765e57f36419053
1enn0 <lehafb15@gmail.com>
2024-09-02 16:25:13 +0200

    make MST model scriptable
    Small refactoring to support exporting the MixStyleTransferModel to TorchScript via `torch.jit.script`

--- mst/panns.py
+++ mst/panns.py
@@ -3,6 +3,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from typing import List
 
 # from torchlibrosa.stft import Spectrogram, LogmelFilterBank
 # from torchlibrosa.augmentation import SpecAugmentation
@@ -24,7 +25,7 @@ def init_bn(bn):
 
 
 class ConvBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, use_batchnorm: bool = True):
+    def __init__(self, in_channels, out_channels, use_batchnorm: bool = True, pool_type: str = 'avg'):
         super(ConvBlock, self).__init__()
         self.use_batchnorm = use_batchnorm
 
@@ -53,6 +54,18 @@ class ConvBlock(nn.Module):
             self.bn1 = nn.Identity()
             self.bn2 = nn.Identity()
 
+        if pool_type == "max":
+            self.pool_fn = F.max_pool2d
+        elif pool_type == "avg":
+            self.pool_fn = F.avg_pool2d
+        elif pool_type == "avg+max":
+            def pool_avg_max(x: torch.Tensor, kernel_size: List[int]):
+                return F.avg_pool2d(x, kernel_size) + F.max_pool2d(x, kernel_size)
+            self.pool_fn = pool_avg_max
+        else:
+            raise Exception("Incorrect argument for `pool_type`!")
+        
+
         self.init_weight()
 
     def init_weight(self):
@@ -63,20 +76,11 @@ class ConvBlock(nn.Module):
             init_bn(self.bn1)
             init_bn(self.bn2)
 
-    def forward(self, input, pool_size=(2, 2), pool_type="avg"):
+    def forward(self, input: torch.Tensor, pool_size: List[int]):
         x = input
         x = F.relu_(self.bn1(self.conv1(x)))
         x = F.relu_(self.bn2(self.conv2(x)))
-        if pool_type == "max":
-            x = F.max_pool2d(x, kernel_size=pool_size)
-        elif pool_type == "avg":
-            x = F.avg_pool2d(x, kernel_size=pool_size)
-        elif pool_type == "avg+max":
-            x1 = F.avg_pool2d(x, kernel_size=pool_size)
-            x2 = F.max_pool2d(x, kernel_size=pool_size)
-            x = x1 + x2
-        else:
-            raise Exception("Incorrect argument!")
+        x = self.pool_fn(x, pool_size)
 
         return x
 
@@ -181,17 +185,17 @@ class Cnn14(nn.Module):
         # if self.training:
         #    x = self.spec_augmenter(x)
 
-        x = self.conv_block1(x, pool_size=(2, 2), pool_type="avg")
+        x = self.conv_block1(x, pool_size=(2, 2))
         # x = F.dropout(x, p=0.2, training=self.training)
-        x = self.conv_block2(x, pool_size=(4, 4), pool_type="avg")
+        x = self.conv_block2(x, pool_size=(4, 4))
         # x = F.dropout(x, p=0.2, training=self.training)
-        x = self.conv_block3(x, pool_size=(4, 2), pool_type="avg")
+        x = self.conv_block3(x, pool_size=(4, 2))
         # x = F.dropout(x, p=0.2, training=self.training)
-        x = self.conv_block4(x, pool_size=(4, 2), pool_type="avg")
+        x = self.conv_block4(x, pool_size=(4, 2))
         # x = F.dropout(x, p=0.2, training=self.training)
-        x = self.conv_block5(x, pool_size=(4, 2), pool_type="avg")
+        x = self.conv_block5(x, pool_size=(4, 2))
         # x = F.dropout(x, p=0.2, training=self.training)
-        x = self.conv_block6(x, pool_size=(2, 2), pool_type="avg")
+        x = self.conv_block6(x, pool_size=(2, 2))
         # x = F.dropout(x, p=0.2, training=self.training)
         x = torch.mean(x, dim=2)  # mean across stft bins
 
